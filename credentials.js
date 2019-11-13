514acaa24ae709dfa9e01f5cbec8cd94a7d4ac54
marktomson981 <56449050+marktomson981@users.noreply.github.com>
2019-11-13 13:28:05 +0100

    sviluppo

---
+++ credentials.js
@@ -0,0 +1,13 @@
+module.export = {
+  mongo: {
+    //..
+  },
+  authProviders: {
+    facebook: {
+      development: {
+        appId: 'your_app_id',
+        appSecret: 'your_app_secret',
+      },
+    },
+  },
+}
--- reliquum.js
+++ reliquum.js
@@ -2,6 +2,36 @@ var express = require('express');
 var app = express();
 app.set('port', process.env.PORT || 3000);
 
+// link API into pipeline
+// currently commented out to reduce console noise
+//app.use(vhost('api.*', rest.rester(apiOptions)));
+
+
+
+
+
+
+
+
+
+
+//Error problem
+//Senza questa parte di codice il sito funziona senza autenticazione
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 app.get('/', function(req, res){
@@ -19,15 +49,70 @@ app.get('/manuale', function(req, res){
 app.get('/info', function(req, res){
  res.render('info');
 });
-
+app.get('/jquery-test', function(req, res){
+ res.render('jquery-test');
+});
 app.get('/favicon', function(req, res){
  res.render('favicon');
 });
+app.get('/nursery-rhyme', function(req, res){
+	res.render('nursery-rhyme');
+});
+app.get('/ringraziamento', function(req, res){
+	res.render('ringraziamento');
+});
+
+app.get('/data/nursery-rhyme', function(req, res){
+	res.json({
+		animal: 'squirrel',
+		bodyPart: 'tail',
+		adjective: 'bushy',
+		noun: 'heck',
+	});
+});
+
+
+// middleware to add weather data to context
+app.use(function(req, res, next){
+ if(!res.locals.partials) res.locals.partials = {};
+   res.locals.partials.weatherContext = getWeatherData();
+   next();
+});
+
+app.use(require('body-parser')());
+
+
+app.use(require('body-parser')());
+app.get('/newsletter', function(req, res){
+ // we will learn about CSRF later...for now, we just
+ // provide a dummy value
+ res.render('newsletter', { csrf: 'CSRF token goes here' });
+});
+app.post('/process', function(req, res){
+ console.log('Form (from querystring): ' + req.query.form);
+ console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+ console.log('Name (from visible form field): ' + req.body.name);
+ console.log('Email (from visible form field): ' + req.body.email);
+ res.redirect(303, '/ringraziamento');
+});
+
+
+
+
 
 
 // set up handlebars view engine
-var handlebars = require('express3-handlebars')
- .create({ defaultLayout:'main' });
+var handlebars = require('express3-handlebars').create({
+ defaultLayout:'main',
+ helpers: {
+ section: function(name, options){
+ if(!this._sections) this._sections = {};
+ this._sections[name] = options.fn(this);
+ return null;
+ }
+ }
+});
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
@@ -81,11 +166,10 @@ function getWeatherData(){
 
 
 
-// middleware to add weather data to context
-app.use(function(req, res, next){
- if(!res.locals.partials) res.locals.partials = {};
-   res.locals.partials.weatherContext = getWeatherData();
-   next();
+app.get('/account', function(req, res){
+ if(!req.session.passport.user)
+ return res.redirect(303, '/unauthorized');
+ res.render('account');
 });
 
 
